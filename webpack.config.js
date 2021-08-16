2fb1f31d06d22fb38fb1a1eaecf259ade323e20f
Aidar <aidar@mircod.com>
2021-08-16 22:20:39 +0300

    Clean webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,11 @@ module.exports = {
   module: {
     rules: [
       {
-        /* The following line to ask babel 
-             to compile any file with extension
-             .js */
         test: /\.(ts|js)?$/,
-        /* exclude node_modules directory from babel. 
-            Babel will not compile any files in this directory*/
         exclude: /node_modules/,
-        // To Use babel Loader
         loader: "babel-loader",
         options: {
-          presets: [
-            "@babel/preset-env" /* to transfer any advansed ES to ES5 */,
-            "@babel/preset-react",
-          ], // to compile react to ES5
+          presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
       {
