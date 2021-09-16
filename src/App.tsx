526f24a7f66d2f4c1e5da560ddf0a688268098b0
Aidar <aidar@mircod.com>
2021-09-16 22:38:26 +0300

    Styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import Board from '@asseinfo/react-kanban';
-import '@asseinfo/react-kanban/dist/styles.css'
+import Board from "@asseinfo/react-kanban";
+import "@asseinfo/react-kanban/dist/styles.css";
 
 const board = {
   columns: [
     {
       id: 1,
-      title: 'Backlog',
+      title: "Backlog",
       cards: [
         {
           id: 1,
-          title: 'Add card',
-          description: 'Add capability to add a card in a column'
+          title: "Add card",
+          description: "Add capability to add a card in a column",
         },
-      ]
+      ],
     },
     {
       id: 2,
-      title: 'Doing',
+      title: "Doing",
       cards: [
         {
           id: 2,
-          title: 'Drag-n-drop support',
-          description: 'Move a card between the columns'
+          title: "Drag-n-drop support",
+          description: "Move a card between the columns",
         },
-      ]
-    }
-  ]
-}
+      ],
+    },
+    {
+      id: 3,
+      title: "Done",
+      cards: [
+        {
+          id: 3,
+          title: "Test card",
+          description: "Just a card for test",
+        },
+      ],
+    },
+  ],
+};
 const App = () => {
   return (
-    <div className="kanban-app">
+    <div
+      className="kanban-app"
+      style={{ background: "yellow", margin: "1em", padding: "1em" }}
+    >
+      Kanban App
       <Board initialBoard={board} />
     </div>
   );
-}
+};
 
 export default App;
